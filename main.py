4c5c1c38a25019e7cbce71dbabeb2b966948d135
Minaguii <guillerminawilder@gmail.com>
2024-05-01 22:13:38 -0300

    wip

---
+++ main.py
@@ -0,0 +1,39 @@
+from tadGarage import *
+import datetime
+
+"""
+Crear lista autos
+Crear lista registro
+Menu
+    Ingresar auto
+        agregarAuto
+    Modificar Auto
+    Eliminar auto
+    Egresar auto
+
+"""
+listaEst = crearEst()
+regisEst = crearEst()
+
+#menu
+while True:
+    print("Bienvenido! Ingrese la opcion a realizar: ")
+    print("1) Ingresar vehiculo.")
+    print("2) Modificar vehiculo.")
+    print("3) Eliminar vehiculo.")
+    print("4) Registrar salida vehiculo.")
+    print("5) Lista de vehiculos en estacionamiento.")
+    print("6) Registro de vehiculos egresados.")
+    print("7) Informe de reacaudacion por torre.")
+    print("8) Cantidad de vehiculos en horas pico.")
+    print("9) Eliminar vehiculos a partir de hora determinada.")
+    print("10) Generar cola por torre.")
+    print("0) Salir.")
+    opcion = 0
+    if 
+    p=input("Ingrese la patente: ")
+    i=datetime.datetime.now()
+    e=0
+    t=int(input("Ingrese el numero de torre: "))
+    ingresarAuto(p,i,e,t)
+
