d0e306e2881f05d30cb82fa2841e2660fc37cacd
nico <bienicolas@gmail.com>
2024-05-01 22:12:45 -0300

    creado TAD Garage

---
+++ tadGarage.py
@@ -0,0 +1,21 @@
+from tadAuto import *
+
+
+
+def crearEst()
+    #crear estacionamiento
+    return []
+
+def ingresarAuto(estacionamiento, p, hi, t):
+    #pide por teclado dato de patente, dueño, hora de entrada y usa crearAuto() para luego ingresarlo al estacionamiento
+    a = crearAuto(p, hi , NULL, t)
+    estacionamiento.append(a)
+
+
+def buscarAuto(patente, estacionamiento):
+    #usa el valor alfanumérico patente para buscar (recorre la lista)  en un estacionamiento y devuelve la posición en la lista (usa devolverPat(auto) )
+
+def eliminarAuto(patente, estacionamiento):
+    #buscar auto por patente y luego lo elimina de la lista
+
+
