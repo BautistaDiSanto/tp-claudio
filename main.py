2cbdd022786211c586e2506221490161358a02a0
Bautista Di Santo <juanbautistadisanto@gmail.com>
2024-06-02 14:05:46 -0300

    add reporde de vehculos en horario pico

--- main.py
+++ main.py
@@ -3,20 +3,19 @@ from tadGarage import *
 from helper import *
 import datetime
 
-#listaEst = crearEst() # crea lista de autos para agregar ingresos
-#regisEst = crearEst() # crea registro de autos para cargar luego los que egresan
-#Funciones
-
 autosPico = 0
 
 valorHora = int(input("Ingrese el valor de la hora: "))
 
 #menu
 opcion = None
+print("")
 print("Creando torres...")
 estacionamiento = crearEst()
-registro = crearEst()		#esto al final va, no?
-print("Bienvenido! Ingrese la opcion a realizar: ")
+registro = crearEst()
+print("")
+print("Bienvenido!")
+
 while opcion != 0:
     print("\n1) Ingresar vehiculo.")
     print("2) Modificar vehiculo.")
@@ -29,57 +28,62 @@ while opcion != 0:
     print("9) Eliminar vehiculos a partir de hora determinada.")
     print("10) Generar cola por torre.")
     print("0) Salir.")
-    opcion = int(input())
+    opcion = int(input("Ingrese la opcion: "))
 
     if opcion == 1:
+        print("")
         print("INGRESAR VEHICULO")
         patente=input("Ingrese la patente: ")
         ingreso=datetime.datetime.now()
+
         if (int(ingreso.hour) >= 7 and int(ingreso.hour) <= 10) or (int(ingreso.hour) >= 17 and int(ingreso.hour) <= 20):
             autosPico += 1
-        #para modificar hora se usa: i = i.replace(hour=int(input()))
-        egreso=0
+
         torre=int(input("Ingrese el numero de torre: "))
-        #ingresarAuto(listaEst,patente,ingreso,egreso,torre) //agrega auto a la lista
-        # ingresarAuto(estacionamiento, patente, ingreso, None, None, torre
-        #Esto cambió
         auto = createCar()
         fillCar(auto, patente, ingreso, None, None, torre)
         ingresarAuto(estacionamiento, auto)
 
     elif opcion == 2:
+        print("")
         print("MODIFICACION DE VEHICULO")
         patente = input("Ingrese patente sin espacios: ")
         j = 0	#posición en estacionamiento
 
         while j < tamanioGar(estacionamiento):
             a = devolverAuto(estacionamiento, j)
-            #if true me quedo con el indice y corto el bucle
+
             if (getPatente(a) == patente):
                 break 	#???
+
+            j += 1
 		
         a = devolverAuto(estacionamiento, j)
+
         while j != None:
-            print("Ingrese la opcion a realizar: ")
+            print("")
             print("1) Modificar hora de ingreso.")
             print("2) Modificar torre donde se estaciona.")
             print("0) Volver.")
-            opcionMod = int(input())
+            opcionMod = int(input("Ingrese la opcion a realizar: "))
 
             if opcionMod == 1:
+                print("")
                 current_time = getHoraIngreso(a)
-
                 updated_time = current_time.replace(hour=int(input("Ingrese nueva hora (0-23): ")), minute=int(input("Ingrese nuevos minutos (0-59): ")))
-
                 setHoraIngreso(a, updated_time)
-                print(f"Hora de ingreso modificada a: {updated_time}")
+
+                if (int(getHoraIngreso(a).hour) >= 7 and int(getHoraIngreso(a).hour) <= 10) or (int(getHoraIngreso(a).hour) >= 17 and int(getHoraIngreso(a).hour) <= 20):
+                    autosPico += 1
 
             elif opcionMod == 2:
                 new_torre = int(input("Ingrese torre: "))
                 setTorre(a, new_torre)
+
             elif opcionMod == 0:
                 print(f"Modificaciones: {a}")
                 break
+
             else:
                 print("El numero ingresado es incorrecto.")
     
@@ -158,6 +162,7 @@ while opcion != 0:
         #calcularTorre(t)
 
     elif opcion == 8:
+        print("")
         print("CANTIDAD DE VEHICULOS EN HORAS PICO")
         print("Entre las 7:00 hasta las 10:00hrs y 17:00 hasta las 20:00hrs ingresaron un total de:")
         print(autosPico)
